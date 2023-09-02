fc440251518c6cda775e9a9a7c1edd5aba1f6db7
ABU Rayhan <rayhan@ibos.io>
2023-09-02 14:08:58 +0600

    put, delete method add

--- SQLiteWebAPI/Controllers/UserController.cs
+++ SQLiteWebAPI/Controllers/UserController.cs
@@ -47,5 +47,56 @@ namespace SQLiteWebAPI.Controllers
         {
             return Ok(await _context.Users.ToListAsync());
         }
+
+        [HttpGet]
+        [Route("GetAllUserById")]
+        public async Task<IActionResult> GetAllUserById(int id)
+        {
+            return Ok(await _context.Users.FirstOrDefaultAsync(x=>x.Id == id));
+        }
+
+        [HttpPut]
+        [Route("UpdateUser")]
+        public async Task<IActionResult> GetAllUserById(User user)
+        {
+            try
+            {
+                if (user is not null && user.Id > 0)
+                {
+                    _context.Users.Update(user);
+                    await _context.SaveChangesAsync();
+
+                    return Ok(user);
+                }
+                else
+                {
+                    return BadRequest("invalid data");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpDelete]
+        [Route("DeleteUserById")]
+        public async Task<IActionResult> DeleteUserById(int id)
+        {
+            User user = await _context.Users.FirstOrDefaultAsync(x => x.Id == id);
+
+            if(user is not null)
+            {
+                _context.Users.Remove(user);
+                await _context.SaveChangesAsync();
+                return Ok(user);
+            }
+            else
+            {
+                return BadRequest("Remove operation is not possible");
+            }
+           
+        }
     }
 }
